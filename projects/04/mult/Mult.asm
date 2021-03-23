26018a37a4a0dd425770c1ac2dee35be8a012724
koobot <the.koo.is.tall@gmail.com>
2021-03-23 12:32:01 +1100

    tested and working - but something in code I don't understand

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,52 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    // x * n = { x + ... + x } n times
+    // initialise vars
+    @R0
+    D=M
+    @x  // x = R0
+    M=D
+
+    @R1
+    D=M
+    @n
+    M=D // n = R1
+    
+    @R2
+    M=0 // R2 = 0 or else program doesn't work - not sure why STOP isn't sufficient
+
+    @i
+    M=0 // i = 0
+    @mult
+    M=0 // mult = 0
+
+(LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @STOP
+    D; JEQ  // if i == n goto STOP
+
+    @mult
+    D=M
+    @x
+    D=D+M
+    @mult
+    M=D     // mult = mult + x
+    @i
+    M=M+1   // i = i + 1
+    @LOOP
+    0; JMP  // Loop again
+
+(STOP)
+    @mult
+    D=M
+    @R2
+    M=D     // RAM[2] = mult
+
+(END)
+    @END
+    0; JMP
+
